bb1fec187b53f59a4dc84140ec4cba5895a2906d
berkaycirak <berkaycirak_11@hotmail.com>
2024-08-22 12:18:13 +0300

    crud is done with tests

--- basic_crud/programs/basic_crud/src/lib.rs
+++ basic_crud/programs/basic_crud/src/lib.rs
@@ -6,14 +6,84 @@ declare_id!("AGSuh1DgECLSSg4Kh2VhCpaMUF4i4aNYDYJeZUF5Y89U");
 pub mod basic_crud {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create(ctx: Context<Create>,name:String,age:u8) -> Result<()> {
+        ctx.accounts.new_account.set_inner(NewAccount { 
+            name, 
+            age, 
+            bump: ctx.bumps.new_account });
+
+        msg!("Your account is saved: {:?}", ctx.accounts.new_account.key());
+        Ok(())
+    }
+    pub fn update(ctx: Context<Update>,name:String,age:u8) -> Result<()> {
+       ctx.accounts.new_account.age = age;
+       ctx.accounts.new_account.name = name; 
+
+        msg!("Your account is updated: {:?}", ctx.accounts.new_account.key());
+        Ok(())
+    }
+    pub fn delete(ctx: Context<Delete>) -> Result<()> {
+        let data =&mut ctx.accounts.new_account;
+        data.close(ctx.accounts.signer.to_account_info())?;
+        msg!("Your account is deleted: {:?}", ctx.accounts.new_account.key());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(
+        init,
+        payer=signer,
+        space= NewAccount::INIT_SPACE,
+        seeds=[b"data",signer.key.as_ref()],
+        bump
+    )]
+    pub new_account:Account<'info,NewAccount>,
+    #[account(mut)]
+    pub signer:Signer<'info>,
+    pub system_program:Program<'info,System>
+}
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(
+        mut,
+        seeds=[b"data",signer.key.as_ref()],
+        bump=new_account.bump,
+    )]
+    pub new_account:Account<'info,NewAccount>,
+    #[account(mut)]
+    pub signer:Signer<'info>,
+    pub system_program:Program<'info,System>
+
+}
+#[derive(Accounts)]
+pub struct Delete<'info> {
+    #[account(
+        mut,
+        seeds=[b"data",signer.key.as_ref()],
+        bump=new_account.bump,
+    )]
+    pub new_account:Account<'info,NewAccount>,
+    #[account(mut)]
+    pub signer:Signer<'info>,
+    pub system_program:Program<'info,System>
+
+}
+
+#[account]
+pub struct NewAccount{
+    pub name:String,
+    pub age:u8,
+    pub bump:u8
+
+}
+
+impl Space for NewAccount{
+    const INIT_SPACE: usize = 8 + (4+50) + 1 + 1; // discriminator + name(4 + expected max length) + age + bump
+} 
+
+
 
 
 
--- basic_crud/tests/basic_crud.ts
+++ basic_crud/tests/basic_crud.ts
@@ -1,16 +1,43 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { BasicCrud } from "../target/types/basic_crud";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { BasicCrud } from '../target/types/basic_crud';
+import { Keypair } from '@solana/web3.js';
 
-describe("basic_crud", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe('basic_crud', () => {
+	// Configure the client to use the local cluster.
+	anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.BasicCrud as Program<BasicCrud>;
+	const program = anchor.workspace.BasicCrud as Program<BasicCrud>;
+	const wallet = Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+	// it('Is initialized!', async () => {
+	// 	// Add your test here.
+	// 	const tx = await program.methods.create('Berkay', 26).rpc();
+	// 	console.log('Your transaction signature', tx);
+	// });
+
+	it('Read the data', async () => {
+		const data = await program.account.newAccount.all();
+		console.log(data);
+	});
+
+	it('Update Account', async () => {
+		const tx = await program.methods.update('Berkay CRK', 27).rpc();
+		console.log('Your transaction signature', tx);
+	});
+
+	it('Read new data', async () => {
+		const data = await program.account.newAccount.all();
+		console.log(data);
+	});
+
+	it('Delete Account', async () => {
+		const tx = await program.methods.delete().rpc();
+		console.log('Your transaction signature', tx);
+	});
+
+	it('Try reading the data after delete', async () => {
+		const data = await program.account.newAccount.all();
+		console.log(data);
+	});
 });
